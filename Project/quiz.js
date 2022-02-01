343c1d6a82bd581b4d957d9651bed18d21ea1e4e
PaulaRodriguezRomera <75445205+PaulaRodriguezRomera@users.noreply.github.com>
2022-02-01 20:18:57 +0000

    questions added

--- Project/quiz.js
+++ Project/quiz.js
@@ -16,3 +16,36 @@ buildQuiz();
 
 //SHOW RESULTS ON CLICK 
 submitButton.addEventListener('click', showResults); 
+
+//QUESTIONS DISPLAY CREATED WITH OBJECTS AND ARRAYS 
+
+const myQuestions = [
+    {
+        question: "Who invented JavaScript", 
+        answers: {
+            a: "Douglas Crockford", 
+            b: "Sheryl Sandberg", 
+            c: "Brendan Eich"
+        }, 
+        correctAnswer: "c"
+    }, 
+    {
+        question: "Which one of these is a JavaScript package manager", 
+        answers: {
+            a: "Node.js", 
+            b: "TypeScript", 
+            c: "npm"
+        }, 
+        correctAnswer: "c"
+    }, 
+    {
+        question: "Which tool can you use to ensure code quality", 
+        answers: {
+            a: "Angular", 
+            b: "jQuery", 
+            c: "JS", 
+            d: "ESLint"
+        }, 
+        correctAnswer: "d"
+    }, 
+]; 
