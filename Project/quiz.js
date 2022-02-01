336bc5725a995752ffd7916cd907978ed83e953c
PaulaRodriguezRomera <75445205+PaulaRodriguezRomera@users.noreply.github.com>
2022-02-01 20:04:11 +0000

    first attempts

--- Project/quiz.js
+++ Project/quiz.js
@@ -1,1 +1,18 @@
-console.log('first quiz'); 
+//SELECT HTML ELEMENTS AND STORE REFERENCES IN VARIABLES 
+
+const quizContainer = document.getElementById('quiz'); 
+const resultsContainer = document.getElementById('results'); 
+const submitButton = document.getElementById('submit'); 
+
+
+//FUNCTION TO BUILD THE QUIZ 
+function buildQuiz(){}
+
+//FUNCTION TO SHOW RESULTS 
+function showResults(){}
+
+//CALL BUILD FUNCTION 
+buildQuiz(); 
+
+//SHOW RESULTS ON CLICK 
+submitButton.addEventListener('click', showResults); 
