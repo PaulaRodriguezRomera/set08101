3a2bb274966b739db69697b76a6e678878310815
PaulaRodriguezRomera <75445205+PaulaRodriguezRomera@users.noreply.github.com>
2022-02-14 21:18:31 +0000

    string challenge

---
+++ lab 7/functions.js
@@ -0,0 +1,14 @@
+/* Write a JavaScript function to replace each character of a string, supplied as an argument to
+the function, by the next one in the English alphabet, returning the resulting manipulated string */ 
+
+//replace each character of a string 
+// by the next one in the English alphabet 
+//returning the resulting string 
+
+//Write a function 
+//supplied as an argument to the funtion 
+
+
+oneString = 'I am going to replace this string'; 
+console.log (oneString.replace('replace', 'make')); 
+console.log (oneString.replace('string', 'cake')); 
